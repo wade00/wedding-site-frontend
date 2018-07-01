6ad7f6d2d7b4bce3e40515f71ce8f6a347fb5d81
Wade Novak <wade@launchpadlab.com>
2018-07-01 15:29:29 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="coming-soon-container">
+        <h1>COMING SOON</h1>
+        <h2>TARA & WADE</h2>
+        <h2>06.21.2019</h2>
+        <h3>{'👰 ❤️ 🤵'}</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
